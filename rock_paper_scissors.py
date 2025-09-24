bdd9f8fcea5613882a0c8ead130e43750157b0fb
ireriz <128937774+ireriz@users.noreply.github.com>
2025-09-24 08:00:00 -0700

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,11 +10,8 @@ if User_Input not in Selections:
     print("Error")
 
 while True:
- if User_Input == "rock"  and PC_choice == "rock" :
-    print("Draw")
- elif User_Input == "paper"  and PC_choice == "paper" :
-    print("Draw")
- elif User_Input == "scissors"  and PC_choice == "scissors" :
+ 
+ if User_Input == PC_choice: 
     print("Draw")
  elif User_Input == "rock" and PC_choice == "paper":
     print("User has won")
@@ -30,6 +27,10 @@ while True:
     print("PC has won")
  break
 
-print(f"The PC chose {PC_choice}")
+print(f"The PC chose {PC_choice} also")
+
+
+#else:
+  #print("Invalid entry")
 
 
