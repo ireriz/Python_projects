224e0bbc5af4d817449996be85f5234e3d5990c9
ireriz <128937774+ireriz@users.noreply.github.com>
2025-09-24 07:53:32 -0700

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,5 @@
 import random
-Selections = ["rock","paper","scissors"]
+Selections = ("rock","paper","scissors")
 PC_choice = random.choice(Selections)
 #print(PC_choice)
 
@@ -9,25 +9,27 @@ User_Input = User_Input.lower()
 if User_Input not in Selections:
     print("Error")
 
-if User_Input == "rock"  and PC_choice == "rock" :
+while True:
+ if User_Input == "rock"  and PC_choice == "rock" :
     print("Draw")
-elif User_Input == "paper"  and PC_choice == "paper" :
+ elif User_Input == "paper"  and PC_choice == "paper" :
     print("Draw")
-elif User_Input == "scissors"  and PC_choice == "scissors" :
+ elif User_Input == "scissors"  and PC_choice == "scissors" :
     print("Draw")
-elif User_Input == "rock" and PC_choice == "paper":
+ elif User_Input == "rock" and PC_choice == "paper":
     print("User has won")
-elif User_Input == "paper" and PC_choice == "rock":
+ elif User_Input == "paper" and PC_choice == "rock":
     print("PC has won")
-elif User_Input == "scissors" and PC_choice == "paper":
+ elif User_Input == "scissors" and PC_choice == "paper":
     print("User has won")
-elif User_Input == "paper" and PC_choice == "scissors":
+ elif User_Input == "paper" and PC_choice == "scissors":
     print("PC has won")
-elif User_Input == "rock" and PC_choice == "scissors":
+ elif User_Input == "rock" and PC_choice == "scissors":
     print("User has won")
-elif User_Input == "scissors" and PC_choice == "rock":
+ elif User_Input == "scissors" and PC_choice == "rock":
     print("PC has won")
-    
-#else:
-  #print("Invalid entry")
+ break
+
+print(f"The PC chose {PC_choice}")
+
 
